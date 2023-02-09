5593a69ae0a54ec58550c7a9899e26aea308dcaa
brenodanyel <bdls1704@hotmail.com>
2023-02-09 11:48:12 -0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Hello World</h1>} />
+        <Route path="*" element={<h1>404 - Not found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
