228e2e4ea4313ca974d20b95b5980119375c870c
brenodanyel <bdls1704@hotmail.com>
2023-02-09 13:58:47 -0300

    feat: add react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MUIProvider } from './Providers/MUI.Provider';
 import { Home } from './Pages/Home.Page';
+import { Toaster } from 'react-hot-toast';
 
 export function App() {
   return (
-    <MUIProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='*' element={<h1>404 - Not found</h1>} />
-        </Routes>
-      </BrowserRouter>
-    </MUIProvider>
+    <>
+      <MUIProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='*' element={<h1>404 - Not found</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </MUIProvider>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+      />
+    </>
   );
 }
