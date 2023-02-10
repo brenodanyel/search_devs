4ba846602d2b555ae7c0766587eb05e53a735380
brenodanyel <bdls1704@hotmail.com>
2023-02-09 23:35:56 -0300

    feat: add theme toggle

--- src/Providers/MUI.Provider.tsx
+++ src/Providers/MUI.Provider.tsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Button } from '@mui/material';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
-const theme = createTheme({
+const dark = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#8C19D2',
+    },
+    secondary: {
+      main: '#0069CA',
+    },
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        variant: 'contained',
+        fullWidth: true,
+        sx: {
+          height: '100%',
+        },
+      },
+    },
+    MuiTextField: {
+      defaultProps: {
+        fullWidth: true,
+        size: 'small',
+      },
+    },
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          cursor: 'pointer',
+        },
+      },
+    },
+  },
+});
+
+const light = createTheme({
   palette: {
     primary: {
       main: '#8C19D2',
@@ -26,18 +66,54 @@ const theme = createTheme({
         size: 'small',
       },
     },
-  }
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          cursor: 'pointer',
+        },
+      },
+    },
+  },
 });
+
+const themes = {
+  dark,
+  light,
+};
 
 type MuiProviderProps = {
   children: React.ReactNode;
 };
 
+const storedTheme = localStorage.getItem('theme') as keyof typeof themes;
+
 export function MUIProvider(props: MuiProviderProps) {
+  const [currentTheme, setCurrentTheme] = useState<keyof typeof themes>(storedTheme || 'light');
+
+  useEffect(() => {
+    localStorage.setItem('theme', currentTheme);
+  }, [currentTheme]);
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={themes[currentTheme]}>
       <CssBaseline />
       {props.children}
-    </ThemeProvider>
+
+      <Button
+        sx={{
+          backgroundColor: 'primary.main',
+          width: '5em',
+          height: '5em',
+          overflow: 'hidden',
+          borderRadius: '50%',
+          position: 'fixed',
+          right: 16,
+          bottom: 16,
+        }}
+        onClick={() => setCurrentTheme((t) => t === 'light' ? 'dark' : 'light')}
+      >
+        {currentTheme === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
+      </Button>
+    </ThemeProvider >
   );
 }
